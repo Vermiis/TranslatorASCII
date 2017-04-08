134f7afad8bd0afd9d90f4ee3c2a0523d17e2751
Nira <Nira@Vermiis>
2017-04-08 21:51:00 +0200

    Add project files.

---
+++ TranslatorASCII/ASCIIGetter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TranslatorASCII
+{
+    class ASCIIGetter
+    {
+        public static List<int> codes(string tekst)
+        {
+            //string value = "9quali52ty3+-/*";
+            List<int> CodesList=null;
+
+            // Convert the string into a byte[].
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(tekst);
+            foreach (var item in asciiBytes)
+            {
+                
+                CodesList.Add(item);               
+            }
+            return CodesList;
+        }
+
+        public static void Comparator (List<int> val)
+        {
+            foreach (var item in val)
+            {
+                if (item==40)
+                {
+                    Console.WriteLine("Opening bracket");
+                }
+                if (item==41)
+                {
+                    Console.WriteLine("Closing bracket");
+                }
+                if (item==43)
+                {
+                    Console.WriteLine("Add operator");
+                }
+                if (item==44)
+                {
+                    Console.WriteLine("Multiplication operator");
+                }
+                if (item==45)
+                {
+                    Console.WriteLine("Substration operator");
+                }
+                if (item==47)
+                {
+                    Console.WriteLine("Division operator");
+                }
+                if (item>=65 && item<=90)
+                {
+                    Console.WriteLine("duza literka");
+                }
+                else if (item >= 97 && item <= 122)
+                {
+                    Console.WriteLine("mala literka");
+                }
+
+                
+            }
+        }
+    }
+
+
+
+
+}
---
+++ TranslatorASCII/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace TranslatorASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz ciag znakow");
+            var x = Console.ReadLine();
+            // string value = "9quali52ty3+-/*";
+
+            var y =ASCIIGetter.codes(x);
+            foreach (var item in x)
+            {
+                Console.WriteLine(item);
+            }
+            //ASCIIGetter.Comparator(y);
+
+        }
+    }
+}
