80d1e5ef814d4be3504cf6921b5a5a78f26ddb76
Ewelina <Ewelina@DESKTOP41>
2017-04-11 14:34:36 +0200

    test

--- TranslatorASCII/Program.cs
+++ TranslatorASCII/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 
 namespace TranslatorASCII
@@ -7,6 +8,10 @@ namespace TranslatorASCII
     {
         static void Main(string[] args)
         {
+            using (var fileStream = new FileStream(@"c:\file.txt", FileMode.Open, FileAccess.Read))
+            {
+                // read from file
+            }
             string a = "sd4+vv/-4=222.4=3,4";
             Console.WriteLine("Podaj ciag znakow: ");
             string own = "";
